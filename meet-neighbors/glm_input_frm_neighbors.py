be2394411cca898ab2d3301bb243650bae59aaea
mcn3159 <mn3159@nyu.edu>
2024-03-22 11:38:35 -0400

    glm inputs should now include genus and species

--- meet-neighbors/glm_input_frm_neighbors.py
+++ meet-neighbors/glm_input_frm_neighbors.py
@@ -31,9 +31,9 @@ def get_glm_input(query,uniq_neighborhoods_d,neighborhood_res,mmseqs_clust,args)
     if args.from_vfdb:
         # want vf categories in the results so that downstream classification is easier
         neighborhood_res = neighborhood_res[neighborhood_res["query"] == query][['vf_name','vf_id','vf_subcategory','vf_category','vfdb_species','vfdb_genus']]
-        df["vf_name"],df["vf_id"],df["vf_subcategory"],df["vf_category"] = neighborhood_res.iloc[0][0],neighborhood_res.iloc[0][1],neighborhood_res.iloc[0][2],neighborhood_res.iloc[0][3]
+        df["vf_name"],df["vf_id"],df["vf_subcategory"],df["vf_category"],df["vfdb_species"],df["vfdb_genus"] = neighborhood_res.iloc[0][0],neighborhood_res.iloc[0][1],neighborhood_res.iloc[0][2],neighborhood_res.iloc[0][3],neighborhood_res.iloc[0][4],neighborhood_res.iloc[0][5]
     else:
-        df["vf_name"],df["vf_id"],df["vf_subcategory"],df["vf_category"] = "non_vf","non_vf","non_vf","non_vf"
+        df["vf_name"],df["vf_id"],df["vf_subcategory"],df["vf_category"],df["vfdb_species"],df["vfdb_genus"] = "non_vf","non_vf","non_vf","non_vf","non_vf","non_vf"
     #df_name = f"{neighborhood_res.iloc[0][1].replace(' ','_').replace(')','').replace('(','').replace('/','|')}_{str(random.randint(1,10000))}_{neighborhood_name}"
     df_name = query
     df.to_csv(f"{args.out}glm_inputs/{df_name}.tsv",sep='\t',header=False,mode="x")
